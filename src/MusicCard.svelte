17e37d2615d7e2793715e57a58ac95733d691eaf
inxeoz <inxeoz@gmail.com>
2025-05-06 18:11:36 +0530

    player

--- src/MusicCard.svelte
+++ src/MusicCard.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
     import coffin_song from './assets/coffin_song.svg'
     import pause from './assets/pause.svg'
+    import play from './assets/play.svg'
+    import circle from './assets/circle.svg'
+
+    let continue_music = false;
+    let played_bar_width = 100;
+
+    function playMusic() {
+        continue_music = true
+    }
 </script>
 
 <div class="music_card_main global_center_div">
@@ -11,57 +20,89 @@
         </div>
 
         <div class="music_player global_center_div">
-            <img src={pause} alt="pause music">
-            <div class="bar global_center_div">
-                <div class="played_bar">
+
+
+            {#if continue_music}
+
+                <div
+                        class="play_pause global_center_div" on:click={() => {
+                    continue_music = false
+                }}>
+                    <img src={pause} alt="pause music">
                 </div>
-                <div class="bar_point">
+
+
+            {:else }
+
+                <div
+                        class="play_pause global_center_div" on:click={()=> {
+                   continue_music = true
+                }}>
+                    <img src={play} alt="play music">
                 </div>
-                <div class="remain_bar">
-                </div>
+
+            {/if}
+
+
+        <div class="bar global_center_div">
+            <div class="played_bar" style="--played_bar_width: {played_bar_width}px">
+            </div>
+            <div class="bar_point global_center_div">
+                <img src={circle} alt="slider handle">
+            </div>
+            <div class="remain_bar">
             </div>
         </div>
     </div>
+</div>
 
-    <div class="music_info global_font">
-        Coffin prod.@Jaymonbeats
-    </div>
+<div class="music_info global_font">
+    Coffin prod.@Jaymonbeats
+</div>
 </div>
 
 <style>
+
+    .play_pause {
+        max-height: 20px;
+        max-width: 20px;
+    }
+
     .music_info {
         color: #fff;
         font-style: italic;
         font-weight: bold;
     }
-    .music_card_main{
+
+    .music_card_main {
         flex-direction: column;
         gap: 20px;
         padding-bottom: 10px;
     }
+
     .bar {
         height: 4px;
         width: 150px;
     }
 
-    .bar_point {
-        width: 10px;
-        height: 10px;
-        background: #f6e8e8;
-        border-radius: 50%;
-    }
 
     .played_bar {
         height: 4px;
-        width: 40px;
+        width: var(--played_bar_width);
         background: #e28c45;
+        border-bottom-left-radius: 50px;
+        border-top-left-radius: 50px;
+
     }
 
     .remain_bar {
         height: 4px;
-        width: 80px;
+        width: 100%;
         background: #685757;
+        border-bottom-right-radius: 50px;
+        border-top-left-radius: 50px;
     }
+
 
     .music_card_player {
         background: #eb5e28;
@@ -78,7 +119,9 @@
         height: 50px;
         width: 100%;
         background: #000000;
-        gap: 10px;
+        justify-content: space-evenly;
+        gap: 2px;
+
     }
 
 </style>
