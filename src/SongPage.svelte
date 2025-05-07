61df368231f6db2c5e18f4fb20b689e52dc217a0
inxeoz <inxeoz@gmail.com>
2025-05-07 23:49:31 +0530

    songs cards

--- src/SongPage.svelte
+++ src/SongPage.svelte
@@ -3,12 +3,14 @@
 import coffin_song from "./assets/coffin_song.svg";
 import MusicCard from "./MusicCard.svelte";
 import Separator from "./Separator.svelte";
+
+import lost_song from './assets/lost_song.svg'
 </script>
 
 <div class="song_page_main global_center_div">
     <MusicCard poster_svg={coffin_song} />
     <Separator/>
-    <MusicCard poster_svg={coffin_song} />
+    <MusicCard poster_svg={lost_song} />
 </div>
 
 <style>
